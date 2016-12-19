284b38125ef8bc86118a334074c3f9b99e32c090
Seth Kramer <sethaepiuva@gmail.com>
2016-12-18 20:11:19 -0500

    styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -84,7 +84,7 @@ end
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url }
       format.json { head :no_content }
     end
   end
